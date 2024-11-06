17c343238979cf9fa4f82d4c5c42355685a32c2c
AhmadFawwazi <118545579+AhmadFawwazi@users.noreply.github.com>
2024-11-06 20:46:52 +0700

    obj func part 2

--- objectiveFunc.js
+++ objectiveFunc.js
@@ -3,9 +3,14 @@ const size = 5;
 const magicNumber = 315;
 const totalLines = 109; // Total garis yang harus sesuai dengan Magic Number
 
-// Fungsi untuk menghitung objective function
-function evaluate(cube) {
+// Fungsi untuk menghitung objective function dari array 1D
+function evaluate(cubeArray) {
     let matchingLines = 0;
+
+    // Fungsi untuk mengakses elemen dalam array 1D sesuai layer, baris, dan kolom
+    function getValue(layer, row, col) {
+        return cubeArray[layer * 25 + row * 5 + col];
+    }
 
     // Periksa semua baris dan kolom pada setiap layer
     for (let layer = 0; layer < size; layer++) {
@@ -13,14 +18,14 @@ function evaluate(cube) {
             // Hitung jumlah pada setiap baris
             let rowSum = 0;
             for (let j = 0; j < size; j++) {
-                rowSum += cube[layer][i][j];
+                rowSum += getValue(layer, i, j);
             }
             if (rowSum === magicNumber) matchingLines++;
 
             // Hitung jumlah pada setiap kolom
             let colSum = 0;
             for (let j = 0; j < size; j++) {
-                colSum += cube[layer][j][i];
+                colSum += getValue(layer, j, i);
             }
             if (colSum === magicNumber) matchingLines++;
         }
@@ -31,8 +36,8 @@ function evaluate(cube) {
         let mainDiagonalSum = 0;
         let secondaryDiagonalSum = 0;
         for (let i = 0; i < size; i++) {
-            mainDiagonalSum += cube[layer][i][i];
-            secondaryDiagonalSum += cube[layer][i][size - i - 1];
+            mainDiagonalSum += getValue(layer, i, i);
+            secondaryDiagonalSum += getValue(layer, i, size - i - 1);
         }
         if (mainDiagonalSum === magicNumber) matchingLines++;
         if (secondaryDiagonalSum === magicNumber) matchingLines++;
@@ -43,7 +48,7 @@ function evaluate(cube) {
         for (let j = 0; j < size; j++) {
             let pillarSum = 0;
             for (let layer = 0; layer < size; layer++) {
-                pillarSum += cube[layer][i][j];
+                pillarSum += getValue(layer, i, j);
             }
             if (pillarSum === magicNumber) matchingLines++;
         }
@@ -56,16 +61,16 @@ function evaluate(cube) {
     let spaceDiagonal4 = 0;
 
     for (let i = 0; i < size; i++) {
-        spaceDiagonal1 += cube[i][i][i];
-        spaceDiagonal2 += cube[i][i][size - i - 1];
-        spaceDiagonal3 += cube[i][size - i - 1][i];
-        spaceDiagonal4 += cube[i][size - i - 1][size - i - 1];
+        spaceDiagonal1 += getValue(i, i, i);
+        spaceDiagonal2 += getValue(i, i, size - i - 1);
+        spaceDiagonal3 += getValue(i, size - i - 1, i);
+        spaceDiagonal4 += getValue(i, size - i - 1, size - i - 1);
     }
     if (spaceDiagonal1 === magicNumber) matchingLines++;
     if (spaceDiagonal2 === magicNumber) matchingLines++;
     if (spaceDiagonal3 === magicNumber) matchingLines++;
     if (spaceDiagonal4 === magicNumber) matchingLines++;
 
-    // Nilai objective function adalah jumlah garis yang mencapai Magic Number
     return matchingLines;
 }
+
