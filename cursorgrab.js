86e9b08e24ad5759c95d8d1258e4f31b62ef196a
FaishalPutra <emfaishalpe@gmail.com>
2024-11-11 13:34:54 +0700

    upd

--- cursorgrab.js
+++ cursorgrab.js
@@ -1,34 +1,40 @@
 const cube = document.getElementById('cubebeng');
 
+let is_drag = false;
+let start_horiz, start_vert;
+let current_rotation_x = 0; 
+let current_rotation_y = 0; 
 
-let isDragging = false;
-let startX, startY;
-let currentRotationX = 30; 
-let currentRotationY = 45; 
-
-
+// mouse ditekan
 cube.addEventListener('mousedown', (e) => {
-    isDragging = true;
-    startX = e.clientX;
-    startY = e.clientY;
-    cube.style.cursor = 'grabbing';
+    is_drag = true;
+    start_horiz = e.clientX;
+    start_vert = e.clientY;
+    cube.style.cursor = 'grabbing'; 
 });
 
-
+// pergerakan mouse
 document.addEventListener('mousemove', (e) => {
-    if (!isDragging) return; // Hanya rotasi jika drag terjadi
+    if (!is_drag) return; // Hanya rotasi jika drag terjadi
 
-    const deltaX = e.clientX - startX;
-    const deltaY = e.clientY - startY;
-    currentRotationY += deltaX * 0.5;
-    currentRotationX -= deltaY * 0.5;
-    cube.style.transform = `rotateX(${currentRotationX}deg) rotateY(${currentRotationY}deg)`;
-    startX = e.clientX;
-    startY = e.clientY;
+    // menghitung jarak posisi mouse
+    const range_x = e.clientX - start_horiz;
+    const range_Y = e.clientY - start_vert;
+
+    // mengatur pergerakan
+    current_rotation_y += range_x * 0.5; //sensi disini
+    current_rotation_x -= range_Y * 0.5; 
+
+    //mengatur css
+    cube.style.transform = `rotateX(${current_rotation_x}deg) rotateY(${current_rotation_y}deg)`;
+
+    // memperbarui kondisi awal
+    start_horiz = e.clientX;
+    start_vert = e.clientY;
 });
 
-// menghentikan dragging saat mouse lepas
+// mouse dilepas
 document.addEventListener('mouseup', () => {
-    isDragging = false;
-    cube.style.cursor = 'grab';
+    is_drag = false;
+    cube.style.cursor = 'grab'; 
 });
